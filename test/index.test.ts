9090d1210d078ffae9a278fd841eca3b264ae72c
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2018-08-14 22:34:10 +0200

    Test fix with link filter

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,8 +5,8 @@ import cmd = require('../src');
 describe('Test pika-web-crawler-test script', () => {
   test
     .stdout()
-    .do(() => cmd.run(['https://pikamachu.github.io']))
-    .it('runs crawler on https://pikamachu.github.io', ctx => {
+    .do(() => cmd.run(['-f=kk', 'https://pikamachu.github.io']))
+    .it('runs crawler on https://pikamachu.github.io with kk filter', ctx => {
       expect(ctx.stdout).to.contain('Opening url https://pikamachu.github.io and parsing links');
     });
 });
