0ba681ba99671331511dfcc0b0e0a7a9263dc551
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2020-06-13 15:46:08 +0200

    Changing package name

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,17 @@
-import { expect, test } from '@oclif/test';
+import { expect, test } from "@oclif/test";
 
-import cmd = require('../src');
+import cmd = require("../src");
 
-describe('Test pkcrawl script', () => {
+describe("Test wcrawl script", () => {
   test
     .stdout()
-    .do(() => cmd.run(['-f=pikamachu.github', 'https://pikamachu.github.io']))
-    .it('runs crawler on https://pikamachu.github.io with "pikamachu.github" filter', ctx => {
-      expect(ctx.stdout).to.contain('Opening url https://pikamachu.github.io and parsing links');
-    });
+    .do(() => cmd.run(["-f=pikamachu.github", "https://pikamachu.github.io"]))
+    .it(
+      'Runs web crawler on https://pikamachu.github.io with "pikamachu.github" filter',
+      ctx => {
+        expect(ctx.stdout).to.contain(
+          "Opening url https://pikamachu.github.io and parsing links"
+        );
+      }
+    );
 });
